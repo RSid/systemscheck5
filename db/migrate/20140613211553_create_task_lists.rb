4c398050cb3fedc0587f32bb236492dd1aa01e8c
RSid <sm1th303@gmail.com>
2014-06-13 17:25:30 -0400

    Initial commit

---
+++ db/migrate/20140613211553_create_task_lists.rb
@@ -0,0 +1,6 @@
+class CreateTaskLists < ActiveRecord::Migration
+  def change
+    create_table :task_lists do |t|
+    end
+  end
+end
---
+++ db/migrate/20140613211617_create_user_task_lists.rb
@@ -0,0 +1,6 @@
+class CreateUserTaskLists < ActiveRecord::Migration
+  def change
+    create_table :user_task_lists do |t|
+    end
+  end
+end
